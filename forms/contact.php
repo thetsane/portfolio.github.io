4788223756d17c4c28eda11befdcd63a51ca0ac4
Morapeli Thetsane <99790492+thetsane@users.noreply.github.com>
2024-05-31 10:45:06 +0200

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -16,16 +16,6 @@
   $contact->from_email = $_POST['email'];
   $contact->subject = $_POST['subject'];
 
- 
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
-
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
   $contact->add_message( $_POST['message'], 'Message', 10);
